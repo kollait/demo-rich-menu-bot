c2b5d96a9aa9d54caf8b23f549d0b85cd6d76363
kollait <56392265+kollait@users.noreply.github.com>
2019-10-14 15:56:19 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,9 @@
-curl -v -X POST https://api.line.me/oauth2/v2.1/token \
--H 'Content-Type: application/x-www-form-urlencoded' \
--d 'grant_type=authorization_code' \
--d 'code=xxx' \
--d 'redirect_uri=xxx' \
--d 'client_id=xxx' \
--d 'client_secret=xxx'
+
+{
+    "access_token": "Ph9Iw2Y6PiN0BrG98j8gxgELX8Zz1+DwFV1X9yubXB7H62CiOtFDydszhJh5jR6wP5UyUabKOdNNz6vL4GDQ9QdI0ssu9FnObIMDnDBt/x65rcsQbdWx/UXRpyrxla3+maixKZGebebuDjB+/ZW0V1GUYhWQfeY8sLGRXgo3xvw=",
+    "expires_in": 2592000,
+    "id_token": "U086a592e679941941fbaf3c98386d8d2",
+    "refresh_token": "Aa1FdeggRhTnPNNpxr8p",
+    "scope": "profile",
+    "token_type": "Bearer"
+}
