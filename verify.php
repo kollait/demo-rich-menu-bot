c9cdd8a266ec02f61ed87791c888557505329151
kollait <56392265+kollait@users.noreply.github.com>
2019-10-14 15:54:16 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,9 +1,7 @@
-<?php 
-  $access_token = 'Ph9Iw2Y6PiN0BrG98j8gxgELX8Zz1+DwFV1X9yubXB7H62CiOtFDydszhJh5jR6wP5UyUabKOdNNz6vL4GDQ9QdI0ssu9FnObIMDnDBt/x65rcsQbdWx/UXRpyrxla3+maixKZGebebuDjB+/ZW0V1GUYhWQfeY8sLGRXgo3xvw=';
-  $url = 'https://api.line.me/oauth2/v2.1/token';
-  $headers = array('Authorization: Bearer ' . $access_token);
-  $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  $result = curl_exec($ch);curl_close($ch);
-  echo $result;
+curl -v -X POST https://api.line.me/oauth2/v2.1/token \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-d 'grant_type=authorization_code' \
+-d 'code=xxx' \
+-d 'redirect_uri=xxx' \
+-d 'client_id=xxx' \
+-d 'client_secret=xxx'
