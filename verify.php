14a41f57e42d7b49d19f9e433a2b45f3102c6a66
kollait <56392265+kollait@users.noreply.github.com>
2019-10-14 16:10:08 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,9 +1,9 @@
-
-{
-    "access_token": "Ph9Iw2Y6PiN0BrG98j8gxgELX8Zz1+DwFV1X9yubXB7H62CiOtFDydszhJh5jR6wP5UyUabKOdNNz6vL4GDQ9QdI0ssu9FnObIMDnDBt/x65rcsQbdWx/UXRpyrxla3+maixKZGebebuDjB+/ZW0V1GUYhWQfeY8sLGRXgo3xvw=",
-    "expires_in": 2592000,
-    "id_token": "U086a592e679941941fbaf3c98386d8d2",
-    "refresh_token": "Aa1FdeggRhTnPNNpxr8p",
-    "scope": "profile",
-    "token_type": "Bearer"
-}
+<?php 
+  $access_token = 'Ph9Iw2Y6PiN0BrG98j8gxgELX8Zz1+DwFV1X9yubXB7H62CiOtFDydszhJh5jR6wP5UyUabKOdNNz6vL4GDQ9QdI0ssu9FnObIMDnDBt/x65rcsQbdWx/UXRpyrxla3+maixKZGebebuDjB+/ZW0V1GUYhWQfeY8sLGRXgo3xvw=';
+  $url = 'https://api.line.me/v2.1/oauth/verify';
+  $headers = array('Authorization: Bearer ' . $access_token);
+  $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);curl_close($ch);
+  echo $result;
