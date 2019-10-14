53b10006cdb5c2636676cc58883139f5cc957c07
kollait <56392265+kollait@users.noreply.github.com>
2019-10-14 16:06:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require "vendor/autoload.php";
 $access_token = 'Ph9Iw2Y6PiN0BrG98j8gxgELX8Zz1+DwFV1X9yubXB7H62CiOtFDydszhJh5jR6wP5UyUabKOdNNz6vL4GDQ9QdI0ssu9FnObIMDnDBt/x65rcsQbdWx/UXRpyrxla3+maixKZGebebuDjB+/ZW0V1GUYhWQfeY8sLGRXgo3xvw=';
 $channelSecret = '5a620251ba162a0a6c51ad2b11c8dd85';
-$idPush = '1653324291'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
