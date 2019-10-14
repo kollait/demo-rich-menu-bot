4ac353b5bccbaaaa888e63e0b5f0e11a143429bc
kollait <56392265+kollait@users.noreply.github.com>
2019-10-14 16:13:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,6 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'Ph9Iw2Y6PiN0BrG98j8gxgELX8Zz1+DwFV1X9yubXB7H62CiOtFDydszhJh5jR6wP5UyUabKOdNNz6vL4GDQ9QdI0ssu9FnObIMDnDBt/x65rcsQbdWx/UXRpyrxla3+maixKZGebebuDjB+/ZW0V1GUYhWQfeY8sLGRXgo3xvw=
+';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
